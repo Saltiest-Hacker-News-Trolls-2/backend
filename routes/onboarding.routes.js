20c8a005d8a03845fdfa9f971d89a38f894a524b
WStrellis <wstrellis@gmail.com>
2019-11-18 21:03:16 -0500

    changed error messages an updated tests

--- routes/onboarding.routes.js
+++ routes/onboarding.routes.js
@@ -39,16 +39,14 @@ router.post(
         const constraint = err.constraint
         if (/username_unique/i.test(constraint)) {
           res.status(400).json({
-            errors: [{ username: 'Sorry, that username is unavailable.' }]
+            errors: { username: 'Sorry, that username is unavailable.' }
           })
         } else if (/email_unique/i.test(constraint)) {
           res.status(400).json({
-            errors: [
-              {
-                email:
-                  'An account has already been registered with that email address.'
-              }
-            ]
+            errors: {
+              email:
+                'An account has already been registered with that email address.'
+            }
           })
         }
       } else {
@@ -82,14 +80,12 @@ router.post(
         return
       }
       // return error if username or password is incorrect
-      res
-        .status(400)
-        .json({
-          errors: [
-            { username: 'Invalid Credentials.' },
-            { password: 'Invalid Credentials.' }
-          ]
-        })
+      res.status(400).json({
+        errors: {
+          username: 'Invalid Credentials.',
+          password: 'Invalid Credentials.'
+        }
+      })
     } catch (err) {
       console.log({ err })
       next({ ...err })
